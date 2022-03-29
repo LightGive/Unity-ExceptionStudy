f592a6c914cdb36cb061440a785e373d20bd1906
LightGive <roi.akase@outlook.jp>
2022-03-30 00:04:55 +0900

    テストを追加

--- Assets/_Projects/Tests/StringCalculatorTest.cs
+++ Assets/_Projects/Tests/StringCalculatorTest.cs
@@ -18,6 +18,10 @@ public class StringCalculatorTest
 			Assert.That(val, Is.EqualTo(1));
 		}
 		{
+			var val = stringCalculator.Calc("((1))");
+			Assert.That(val, Is.EqualTo(1));
+		}
+		{
 			var val = stringCalculator.Calc("1+2");
 			Assert.That(val, Is.EqualTo(3));
 		}
@@ -30,12 +34,24 @@ public class StringCalculatorTest
 			Assert.That(val, Is.EqualTo(6));
 		}
 		{
+			var val = stringCalculator.Calc("(1+2+3)");
+			Assert.That(val, Is.EqualTo(6));
+		}
+		{
 			var val = stringCalculator.Calc("(1+1)*(2+2)*(3+3)");
 			Assert.That(val, Is.EqualTo(48));
 		}
 		{
 			var val = stringCalculator.Calc("(1+(2*(3+3)))");
 			Assert.That(val, Is.EqualTo(13));
+		}
+		{
+			var val = stringCalculator.Calc("0.1+0.2+0.3");
+			Assert.That(val, Is.EqualTo(0.6f));
+		}
+		{
+			var val = stringCalculator.Calc(".1+.2*.3");
+			Assert.That(val, Is.EqualTo(0.09f));
 		}
 	}
 }
